77843aa97e69bfd8af70bd6d2098cff338a0ee19
Shlomo <Shlomo@DESKTOP-IGIOI52>
2022-01-05 17:56:22 +0200

    newBranch

--- College-Mvc-Web-Api/Controllers/API/StudentController.cs
+++ College-Mvc-Web-Api/Controllers/API/StudentController.cs
@@ -9,65 +9,32 @@ using System.Web.Http;
 
 namespace College_Mvc_Web_Api.Controllers.API
 {
+
     public class StudentController : ApiController
     {
-        string connectionString = "Data Source=DESKTOP-IGIOI52;Initial Catalog=College;Integrated Security=True";
+        static string connectionString = "Data Source=DESKTOP-IGIOI52;Initial Catalog=College;Integrated Security=True";
+        CollegeDataContext collegeDB = new CollegeDataContext(connectionString);
         // GET: api/Student
         public IHttpActionResult Get()
         {
             try
             {
-                List<Student> stuList = new List<Student>();
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string sql = $@"SELECT * FROM student";
-                    SqlCommand cmd = new SqlCommand(sql, connection);
-                    SqlDataReader dr = cmd.ExecuteReader();
-                    if (dr.HasRows)
-                    {
-                        while (dr.Read())
-                        {
-                            stuList.Add(new Student(dr.GetString(1), dr.GetString(2), dr.GetDateTime(3), dr.GetString(4), dr.GetInt32(5)));
-                        }
-                    }
-                    connection.Close();
-                    return Ok(new { MESSAGE = "NEW STUDENT HAS BEEN ADDED TO YOUR LIST", stuList });
-                }
+                //List<Student> stuList= new List<Student>();
+                return Ok(collegeDB.students.ToList());
             }
-            catch (Exception err)
+            catch (Exception ex)
             {
-                return Ok(err.Message);
-            }
-            catch
-            {
-                return Ok(new { Message = "GENERAL ERROR" });
+                return BadRequest(ex.Message);
             }
         }
 
         // GET: api/Student/5
         public IHttpActionResult Get(int id)
         {
-            
+
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string query = $@"SELECT * FROM student WHERE Id = {id}";
-                    SqlCommand command = new SqlCommand(query, connection);
-                    SqlDataReader datadb = command.ExecuteReader();
-                    if (datadb.HasRows)
-                    {
-                        while (datadb.Read())
-                        {
-                            Student stu = new Student(datadb.GetString(1), datadb.GetString(2), datadb.GetDateTime(3), datadb.GetString(4), datadb.GetInt32(5));
-                            return Ok(new { stu });
-                        }
-                    }
-                    connection.Close();
-                    return Ok(new { MESSAGE = "NOT EXISST" });
-                }
+                return Ok(collegeDB.students.First(studentItem => studentItem.Id == id));
             }
             catch (Exception err)
             {
@@ -80,83 +47,60 @@ namespace College_Mvc_Web_Api.Controllers.API
         }
 
         // POST: api/Student
-        public IHttpActionResult Post([FromBody] Student value)
+        public IHttpActionResult Post([FromBody] student value)
         {
-         
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string postQuery = $@"INSERT INTO student(Fname,Lname,Birth,Email,Year_study)
-                    values('{value.firstName}','{value.lastName}','{value.Birth}','{value.Email}',{value.StudyYear}) ";
-                    SqlCommand command = new SqlCommand(postQuery, connection);
-                    int rows = command.ExecuteNonQuery();
-                    connection.Close();
-                }
-                return Ok(new { value });
+                collegeDB.students.InsertOnSubmit(value);
+                collegeDB.SubmitChanges();
+                return Ok("sucsses");
             }
             catch (SqlException err)
             {
-                return Ok(err.Message);
+                return BadRequest(err.Message);
             }
-            catch
+            catch (Exception ex)
             {
-                return Ok(new { MESSAGE = "GENERAL ERROR" });
+                return BadRequest(ex.Message);
+
             }
         }
         // PUT: api/Student/5
         [HttpPut]
-        public IHttpActionResult Put(int id, [FromBody] Student value)
+        public IHttpActionResult Put(int id, [FromBody] student value)
         {
-            
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                student updateStudent = collegeDB.students.First(studentItem => studentItem.Id == id);
+                if (updateStudent != null)
                 {
-                    connection.Open();
-                    string putQuery = $@"UPDATE student SET Fname='{value.firstName}',Lname='{value.lastName}',Birth='{value.Birth}',Email='{value.Email}',Year_study={value.StudyYear} 
-                                       WHERE Id = {id}";
-                    SqlCommand sqlCommand = new SqlCommand(putQuery, connection);
-                    int updaterow = sqlCommand.ExecuteNonQuery();
-                    connection.Close();
+                    updateStudent.Fname = value.Fname;
+                    updateStudent.Lname = value.Lname;
+                    updateStudent.Birth=value.Birth;
+                    updateStudent.Email = value.Email;
+                    updateStudent.Year_study = value.Year_study;
                 }
+                collegeDB.SubmitChanges();
+                return Ok("change Saved");
             }
-            catch (Exception err)
+            catch(SqlException err)
             {
-                return Ok(err.Message);
+                return BadRequest(err.Message);
             }
-            catch
+            catch(Exception ex)
             {
-                return Ok(new { MESSAGE = "GENERAL ERROR" });
+                return BadRequest(ex.Message);
             }
-            return Ok();
+
         }
 
         // DELETE: api/Student/5
         public IHttpActionResult Delete(int id)
         {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string deleteQuery = $@"DELETE FROM student WHERE Id = {id}";
-                    SqlCommand deleteCommand = new SqlCommand(deleteQuery, connection);
-                    int deletedaterow = deleteCommand.ExecuteNonQuery();
-                    connection.Close();
-                }
+            collegeDB.students.DeleteOnSubmit(collegeDB.students.First(studentItem => studentItem.Id == id));
+            collegeDB.SubmitChanges();
+            return Ok("OBG WAS DELETED");
 
-            }
-            catch (SqlException err)
-            {
-                return Ok(err.Message);
-            }
-            catch
-            {
-
-            }
-            return Ok();
         }
     }
 }
